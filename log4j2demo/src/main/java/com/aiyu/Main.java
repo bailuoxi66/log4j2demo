ba8ff9d5039e9e1a3f79b8632643092a89716604
luoyu <liangzhengtao.lzt@lazada.com>
2021-12-15 00:03:09 +0800

    feat: log4j2demo add

---
+++ log4j2demo/src/main/java/com/aiyu/Main.java
@@ -0,0 +1,19 @@
+package com.aiyu;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author ：luoyu
+ * @version ：1.0
+ * @date ： 2021/7/17 1:34 上午
+ * @description
+ */
+
+public class Main {
+    static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("log4j2 demo");
+    }
+}
